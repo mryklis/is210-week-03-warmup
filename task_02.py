23f89ef9d0d5d84c93b56a5efe56295e0c054ed7
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-09-09 01:01:12 +0000

    created task_02

---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module creates a variable 'WEEKS' and performs the mathematical operation below"""
+
+WEEKS = ((((19%10) + 100) + (2**8)) / 7 )
+
+print WEEKS
