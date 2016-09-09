81951355bc25d56d905534ba3fe1160c9841aebd
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-09-09 02:35:40 +0000

    added multi line docstring

--- task_09.py
+++ task_09.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+set first variable to 42 and second variable to browning motion
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
