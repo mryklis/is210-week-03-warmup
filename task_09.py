9b4d6b95b60de24653a5e821fdd7f019460cccb3
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-09-10 14:28:27 +0000

    changes made to task 9 and 10

--- task_09.py
+++ task_09.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A computer that can build the question.
+
 set first variable to 42 and second variable to browning motion
+line 2 of comments.
 """
 
 THE_ANSWER_TO_EVERYTHING = 42
